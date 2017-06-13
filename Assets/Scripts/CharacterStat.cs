09b806f7eb84ed698f7591cb0cc16cae3e10e209
Dowzz <michel.barny@outlook.fr>
2017-06-13 16:30:11 +0200

    Stats

---
+++ Assets/Scripts/CharacterStat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStat : MonoBehaviour {
+	public List<BaseStat> stats = new List<BaseStat>();
+
+	void start()
+	{
+		stats.Add(new BaseStat(4, "Power", "Your power level"));
+		stats[0].AddStatBonus(new StatBonus(5));
+		Debug.Log (stats[0].GetCalculatedStat());
+		Debug.Log ("coucou");
+	}
+
+
+
+
+}
